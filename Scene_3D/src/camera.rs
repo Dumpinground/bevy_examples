381028e29f4aef17a3c964ed3d4f2418e53a576d
Ad <DeadLimit@lkr5.onmicrosoft.com>
2024-02-02 13:25:11 +0800

    update

--- Scene_3D/src/camera.rs
+++ Scene_3D/src/camera.rs
@@ -5,7 +5,7 @@ pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_systems(Startup,setup)
-        .add_systems(Update, move_camera);
+        .add_systems(Update, (move_camera, lift_camera));
     }
 }
 
@@ -16,7 +16,7 @@ fn setup(mut commands: Commands) {
     });
 }
 
-fn move_camera(mut query: Query<&mut Transform, With<Camera>>, keyboard: Res<Input<KeyCode>>, mut wheel_event: EventReader<MouseWheel>, time: Res<Time>) {
+fn move_camera(mut query: Query<&mut Transform, With<Camera>>, keyboard: Res<Input<KeyCode>>, time: Res<Time>) {
     let speed = 2.;
 
     if let Ok(mut transform) = query.get_single_mut() {
@@ -35,6 +35,26 @@ fn move_camera(mut query: Query<&mut Transform, With<Camera>>, keyboard: Res<Inp
         if keyboard.pressed(KeyCode::S) {
             direction += Vec3::new(0., 0., -1.);
         }
+
+        // for event in wheel_event.read() {
+        //     match event.unit {
+        //         MouseScrollUnit::Line | MouseScrollUnit::Pixel => direction += Vec3::new(0., event.y * 2., 0.),
+        //     }
+        // }
+
+        if direction.length() > 0. {
+            direction = direction.normalize();
+        }
+
+        transform.translation += direction * speed * time.delta_seconds();
+    }
+}
+
+fn lift_camera(mut query: Query<&mut Transform, With<Camera>>, mut wheel_event: EventReader<MouseWheel>, time: Res<Time>) {
+    let speed = 4.;
+
+    if let Ok(mut transform) = query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
 
         for event in wheel_event.read() {
             match event.unit {
