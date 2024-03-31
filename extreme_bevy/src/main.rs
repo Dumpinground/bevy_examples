7fb72afb40bd4a266bccef97de668d02de2648ba
Ad <DeadLimit@lkr5.onmicrosoft.com>
2024-03-31 21:04:04 +0800

    update

--- extreme_bevy/src/main.rs
+++ extreme_bevy/src/main.rs
@@ -1,16 +1,19 @@
 use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy_matchbox::{matchbox_socket::SingleChannel, MatchboxSocket};
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-            prevent_default_event_handling: false,
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                prevent_default_event_handling: false,
+                ..default()
+            }),
             ..default()
-        }), ..default()
-    }))
-    .insert_resource(ClearColor(Color::rgb(0.53, 0.53, 0.53)))
-    .add_systems(Startup, (setup, spawn_player))
-    .add_systems(Update, move_player)
-    .run();
+        }))
+        .insert_resource(ClearColor(Color::rgb(0.53, 0.53, 0.53)))
+        .add_systems(Startup, (setup, spawn_player, start_matchbox_socket))
+        .add_systems(Update, move_player)
+        .run();
 }
 
 fn setup(mut commands: Commands) {
@@ -23,20 +26,23 @@ fn setup(mut commands: Commands) {
 struct Player;
 
 fn spawn_player(mut commands: Commands) {
-    commands.spawn((SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0., 0.47, 1.),
-            custom_size: Some(Vec2::new(1., 1.)),
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0., 0.47, 1.),
+                custom_size: Some(Vec2::new(1., 1.)),
+                ..default()
+            },
             ..default()
         },
-        ..default()
-    }, Player));
+        Player,
+    ));
 }
 
 fn move_player(
     mut players: Query<&mut Transform, With<Player>>,
     keys: Res<ButtonInput<KeyCode>>,
-    time: Res<Time>
+    time: Res<Time>,
 ) {
     let mut direction = Vec2::ZERO;
     if keys.any_pressed([KeyCode::ArrowUp, KeyCode::KeyW]) {
@@ -61,4 +67,25 @@ fn move_player(
     for mut transform in &mut players {
         transform.translation += move_delta.extend(0.);
     }
+}
+
+fn start_matchbox_socket(mut commands: Commands) {
+    let room_url = "ws://localhost:3536/extreme_bevy?next=2";
+    commands.insert_resource(MatchboxSocket::new_ggrs(room_url))
+}
+
+fn wait_for_players(mut socket: ResMut<MatchboxSocket<SingleChannel>>) {
+    if socket.get_channel(0).is_err() {
+        return;
+    }
+
+    socket.update_peers();
+    let players = socket.players();
+
+    let num_players = 2;
+    if players.len() < num_players {
+        return;
+    }
+
+    info!("All peers have joined, going in-game");
 }
