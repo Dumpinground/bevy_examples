0f9368ed685ab3809de1efbdcba27a1eb4fe4c31
Ad <DeadLimit@lkr5.onmicrosoft.com>
2024-04-25 12:07:48 +0800

    update

--- Scene_3D/src/camera.rs
+++ Scene_3D/src/camera.rs
@@ -15,22 +15,6 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         let envs = crate::env::read_envs();
 
-        match envs.camera {
-            CameraType::Default => {
-                app.add_plugins((MyCameraPlugin, MovablePlugin));
-            }
-            CameraType::Orthographic => {
-                app.add_plugins((OrthographicCameraPlugin, MovablePlugin));
-            }
-            CameraType::PanOrbit => {
-                if let ReferenceType::External = envs.reference {
-                    app.add_plugins(external::panorbit::CameraPlugin);
-                } else {
-                    app.add_plugins(panorbit::CameraPlugin);
-                }
-            }
-        }
-
         match (envs.camera, envs.reference) {
             (CameraType::Orthographic, ReferenceType::Internal) => {
                 app.add_plugins((OrthographicCameraPlugin, MovablePlugin));
